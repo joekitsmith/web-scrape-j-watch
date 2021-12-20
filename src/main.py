1c33bae9f2865296cfd44a4546838e638cdd2629
Joseph Smith <joseph.smith@tessella.com>
2021-12-20 10:44:18 +0000

    Correct backup dates

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from web_scraper.jwatch import JWatchScraper
 from inputs.get_inputs import get_inputs
 
 backup_url = "https://www.jwatch.org/general-medicine-online-archives"
-backup_dates = ["Feb", "2021", "Dec", "2021"]
+backup_dates = ("02", "2021", "12", "2021")
 
 if __name__ == "__main__":
     url, dates = get_inputs()
