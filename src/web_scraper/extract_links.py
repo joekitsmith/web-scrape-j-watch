f1fc6fcc0c874a207cb9d93194ba2e2de809cdd7
Joseph Smith <joseph.smith@tessella.com>
2021-12-20 10:44:05 +0000

    Remove tqdm

--- src/web_scraper/extract_links.py
+++ src/web_scraper/extract_links.py
@@ -1,5 +1,3 @@
-from tqdm import tqdm
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
